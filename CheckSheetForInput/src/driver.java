dbcd40f16e796b10e8383b22e6f57be3d4a1afc5
De La Cruz <S02956711@ccc.ccofc.edu>
2026-02-04 10:24:43 -0700

    inputchecksheet

---
+++ CheckSheetForInput/src/driver.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class driver {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		String word = word.toUpperCase();
+		int intNum;
+		float realNum = 987654321.987654321f;
+		double biggerRealNum;
+		char letter;
+
+		System.out.println("Enter multiple words");
+		word = input.nextLine();
+		System.out.println(word);
+
+		System.out.println("Enter a num");
+		intNum = input.nextInt();
+		System.out.println(intNum);
+
+		System.out.println("Enter a real num");
+		realNum = input.nextFloat();
+		realNum = realNum + realNum;
+		System.out.println(realNum);
+
+		System.out.println("Enter a bigger real num");
+		biggerRealNum = input.nextDouble();
+		biggerRealNum = biggerRealNum + biggerRealNum; 
+		System.out.printf("$%.2f\n", biggerRealNum);
+
+		System.out.println("Enter a single letter");
+		letter = input.next().charAt(0);
+		System.out.println(letter);
+
+	}
+
+}
--- InputOutput/src/MtoKm.java
+++ InputOutput/src/MtoKm.java
@@ -4,22 +4,14 @@ public class MtoKm {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		double miles, kM; 
-		
+		double miles, kM;
+
 		System.out.println("How many miles");
 		miles = input.nextDouble();
-		
+
 		kM = miles * 1.60934;
-		
+
 		System.out.printf("There are %.3f kilometers in %.3f miles", kM, miles);
-		
-		
-		
-		
-		
-		
-		
-		
 
 	}
 
